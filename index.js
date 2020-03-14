5e9e34dc503ce1162f2579dbc7ce7fc62c3b34d2
AirFusion <jfang.cv.ca.us@gmail.com>
2020-03-14 16:40:37 -0700

    update for github hosting

--- index.js
+++ index.js
@@ -20,18 +20,20 @@ function showPosition(position, county, state, found) {
   Http.onreadystatechange = (e) => {
     county = String(JSON.parse(Http.response).results[0].county_name).toUpperCase();
     state = String(JSON.parse(Http.response).results[0].state_code).toUpperCase();
-    // console.log(county)
-    // console.log(state)
-    $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/publicHealth.json", function (data) {
-      for (var i = 0; i < data.length; i++) {
-        if ((data[i].COUNTY === county) && (data[i].STATE === state) && (data[i].NAME.indexOf("PUBLIC HEALTH") != -1)) {
-          console.log(data[i])
-          // document.getElementById("pubHealthDept").src = `https://www.google.com/search?q=%${data[i].WEBSITE}/&btnI=Im+Feeling+Lucky`;
-          // document.getElementById("pubHealthDept").src = `https://www.cracking.com.ar/redir/redir.php?URL=${data[i].WEBSITE}`;
-          document.getElementById("pubHealthDept").src = data[i].WEBSITE;
-          break;
-        }
-      }
-    });
+    console.log(county)
+    console.log(state)
+
+    // $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/publicHealth.json", function (data) {
+    //   for (var i = 0; i < data.length; i++) {
+    //     if ((data[i].COUNTY === county) && (data[i].STATE === state) && (data[i].NAME.indexOf("PUBLIC HEALTH") != -1)) {
+    //       console.log(data[i])
+    //       // document.getElementById("pubHealthDept").src = `https://www.google.com/search?q=%${data[i].WEBSITE}/&btnI=Im+Feeling+Lucky`;
+    //       // document.getElementById("pubHealthDept").src = `https://www.cracking.com.ar/redir/redir.php?URL=${data[i].WEBSITE}`;
+    //       document.getElementById("pubHealthDept").src = data[i].WEBSITE;
+    //       break;
+    //     }
+    //   }
+    // });
+    
   }
 }
