44f92e4eebc89a04b4e8b7e6c764eea2e7cf629b
AirFusion <jfang.cv.ca.us@gmail.com>
2020-03-14 22:58:03 -0700

    need to edit button for popup

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function showPosition(position, county, state, found) {
   // location = position;
   console.log(position)
   const Http = new XMLHttpRequest();
-  const url = `https://geo.fcc.gov/api/census/area?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`;
+  // const url = `https://geo.fcc.gov/api/census/area?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`;
+  const url = `https://geo.fcc.gov/api/census/area?lat=29.774884&lon=-82.424085&format=json`;
   Http.open("GET", url);
   Http.send();
 
@@ -47,6 +48,8 @@ function showPosition(position, county, state, found) {
     $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/stateHealth.json", function (data) {
       if (data[state].HTTPS === false) {
         // notification
+
+        $('#ex1').modal();
         document.getElementById("pubHealthDept").src = "https://www.cdc.gov/coronavirus/"
       } else if (data[state].HTTPS === true) {
         $("#pubHealthDept").remove();
