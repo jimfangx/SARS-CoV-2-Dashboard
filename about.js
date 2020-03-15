544d2bc97c463c2d8ad8e3e71dec2592828ac863
AirFusion <jfang.cv.ca.us@gmail.com>
2020-03-15 03:13:41 -0700

    everything works except for the location spec iframe on main pg. Somehow we've got 2 framed at once stacked

---
+++ about.js
@@ -0,0 +1,38 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position, county, state, found) {
+    console.log(position)
+    const Http = new XMLHttpRequest();
+    const url = `https://geo.fcc.gov/api/census/area?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`;
+    // const url = `https://geo.fcc.gov/api/census/area?lat=29.774884&lon=-82.424085&format=json`;
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        county = String(JSON.parse(Http.response).results[0].county_name).toUpperCase();
+        state = String(JSON.parse(Http.response).results[0].state_code).toUpperCase();
+        stateVal = String(JSON.parse(Http.response).results[0].state_name);
+        var countySelected;
+        $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/stateHealth.json", function (dataState) {
+            $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/publicHealth.json", function (dataCounty) {
+
+
+                if (county === undefined && state === undefined) {
+                    document.getElementById("location").innerHTML = `You are currently not in the United States or minor outlying islands. Please visit the <a href: "https://who.int/coronavirus">WHO's website</a> for more information.`
+                } else {
+                    for (var i=0; i<dataCounty.length; i++) {
+                        if ((dataCounty[i].STATE===state) && (dataCounty[i].COUNTY===county)) {
+                            countySelected=i;
+                            break;
+                        }
+                    }
+                    document.getElementById("location").innerHTML = `You are in ${county}, ${stateVal.toUpperCase()}. Below are your state and county's public health department's information:<br><br><b>${dataState[state].NAME}</b><br>Address: ${dataState[state].ADDRESS}${dataState[state].ADDRESS2==="" ? "":" "+dataState[state].ADDRESS2}, ${dataState[state].CITY}, ${dataState[state].STATE}${dataState[state].ZIP}<br>Telephone (May not be 100% accurate): ${dataState[state].TELEPHONE}<br><br><b>${dataCounty[countySelected].NAME} (May not be 100% accurate)</b><br>Address: ${dataCounty[countySelected].ADDRESS}${dataCounty[countySelected].ADDRESS2==="" ? "":" "+dataCounty[countySelected].ADDRESS2}, ${dataCounty[countySelected].CITY}, ${dataCounty[countySelected].STATE}${dataCounty[countySelected].ZIP}<br>Telephone: ${dataCounty[countySelected].TELEPHONE}`
+                }
+            })
+        })
+    }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var state;
 var found;
 var location;
 var outsideUS;
+var stateVal;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(showPosition);
@@ -14,43 +15,25 @@ function showPosition(position, county, state, found) {
   // location = position;
   console.log(position)
   const Http = new XMLHttpRequest();
-  // const url = `https://geo.fcc.gov/api/census/area?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`;
-  const url = `https://geo.fcc.gov/api/census/area?lat=29.774884&lon=-82.424085&format=json`;
+  const url = `https://geo.fcc.gov/api/census/area?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`;
+  // const url = `https://geo.fcc.gov/api/census/area?lat=29.774884&lon=-82.424085&format=json`;
   Http.open("GET", url);
   Http.send();
 
   Http.onreadystatechange = (e) => {
     county = String(JSON.parse(Http.response).results[0].county_name).toUpperCase();
     state = String(JSON.parse(Http.response).results[0].state_code).toUpperCase();
-    console.log(county)
-    console.log(state)
-    console.log(Http.response)
-    console.log("JSDFLKFJLK" + JSON.parse(Http.response))
-
-    // debugger
-    // if (String(JSON.parse(Http.response).results === "")) {
-    //   outsideUS = true;
-    //   // document.getElementById("pubHealthDept").src = "https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
-    // } else {
-
-    // $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/publicHealth.json", function (data) {
-    //   for (var i = 0; i < data.length; i++) {
-    //     if ((data[i].COUNTY === county) && (data[i].STATE === state) && (data[i].NAME.indexOf("PUBLIC HEALTH") != -1)) {
-    //       console.log(data[i])
-    //       // document.getElementById("pubHealthDept").src = `https://www.google.com/search?q=%${data[i].WEBSITE}/&btnI=Im+Feeling+Lucky`;
-    //       // document.getElementById("pubHealthDept").src = `https://www.cracking.com.ar/redir/redir.php?URL=${data[i].WEBSITE}`;
-    //       document.getElementById("pubHealthDept").src = data[i].WEBSITE;
-    //       break;
-    //     }
-    //   }
-    // });
-
+    stateVal = String(JSON.parse(Http.response).results[0].state_name);
+    // console.log(county)
+    // console.log(state)
+    // console.log(Http.response)
+    // console.log("JSDFLKFJLK" + JSON.parse(Http.response))
     $.getJSON("https://raw.githubusercontent.com/AirFusion45/SARS-CoV-2-Dashboard/master/stateHealth.json", function (data) {
       if (data[state].HTTPS === false) {
-        // notification
-
+        document.getElementById("modalContents").innerHTML = `We were unable to display your state's public health department's website.<br>${stateVal}'s public health department's COVID19 website is <a target="_blank" href="${data[state].WEBSITE}">${data[state].WEBSITE}</a><br>Please visit your local public health department for more information.<br>Error: HTTP & HTTPS mixed active content block. State's website is using HTTP.`
         $('#ex1').modal();
-        document.getElementById("pubHealthDept").src = "https://www.cdc.gov/coronavirus/"
+        $("#pubHealthDept").remove();
+        $(".column-right").append(`<iframe id="pubHealthDeptCustom" src="https://www.cdc.gov/coronavirus/2019-ncov/index.html"></iframe>`)
       } else if (data[state].HTTPS === true) {
         $("#pubHealthDept").remove();
         $(".column-right").append(`<iframe id="pubHealthDeptCustom" is="x-frame-bypass" src="${data[state].WEBSITE}"></iframe>`)
